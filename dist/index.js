f9280c8217c201d883f9e970505ca2575493a635
Diego Alberto Chavarria Ortiz <64041305+dbch-git@users.noreply.github.com>
2026-02-13 22:11:19 +0000

    Update main.js

--- dist/index.js
+++ dist/index.js
@@ -39129,7 +39129,7 @@ WError.prototype.cause = function we_cause(c)
 /***/ 9761:
 /***/ ((module) => {
 
-module.exports = eval("require")("./@actions/core");
+module.exports = eval("require")("@actions/core");
 
 
 /***/ }),
